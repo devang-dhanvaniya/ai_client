2de70c0b321b5fe6e9cac85bc9a847539c9be548
jenish-devandro <jenish@devandro.in>
2025-02-07 15:56:16 +0530

    -add eye off on in pass and chnage login design

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <title>{{ $title ?? '' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -17,7 +17,9 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .navbar {
             transition: all 0.3s ease;
@@ -67,6 +69,37 @@
             transition: color 0.3s;
         }
 
+        .logincard {
+            box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+            padding: 20px;
+            border-radius: 10px;
+            width: 400px
+        }
+
+        .loginmain {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center
+        }
+
+        .login-header {
+            font-size: 20px;
+            font-weight: 600;
+            text-align: center;
+        }
+
+        .eye_posiotin {
+            position: relative;
+        }
+
+        .eye_posiotin span {
+            position: absolute;
+            top: 50%;
+            right: 5px;
+            transform: translate(-50%, -50%)
+        }
+
         @media (max-width: 768px) {
             .navbar-nav {
                 text-align: center;
@@ -91,54 +124,71 @@
 </head>
 
 <body>
-<div class="container-fluid p-0">
+    <div class="container-fluid p-0">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light ps-4 pe-4 shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="/dashboard">AI Client</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ps-4 pe-4 shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="/dashboard">AI Client</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
                     aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}"
-                           href="/dashboard">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('history') ? 'active' : '' }}" href="/history">
-                            Position-History</a>
-                    </li>
-                </ul>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}"
+                                href="/dashboard">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('history') ? 'active' : '' }}" href="/history">
+                                Position-History</a>
+                        </li>
+                    </ul>
 
-                <!-- Navbar Text Section (User Details and Dropdown) -->
-                <div class="navbar-text">
-                    @guest
-                    @else
-                        <div class="dropdown">
-                            <a class="text-decoration-none dropdown-toggle pe-auto" id="dropdownUser"
-                               data-bs-toggle="dropdown">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
-                                <livewire:logout/>
-                            </ul>
-                        </div>
-                    @endguest
+                    <!-- Navbar Text Section (User Details and Dropdown) -->
+                    <div class="navbar-text">
+                        @guest
+                        @else
+                            <div class="dropdown">
+                                <a class="text-decoration-none dropdown-toggle pe-auto" id="dropdownUser"
+                                    data-bs-toggle="dropdown">
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
+                                    <livewire:logout />
+                                </ul>
+                            </div>
+                        @endguest
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <div class="container">
-        {{ $slot }}
+        <div class="container">
+            {{ $slot }}
+        </div>
     </div>
-</div>
-<script data-navigate-once src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    <script data-navigate-once src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        document.getElementById('togglePassword').addEventListener('click', function() {
+            const passwordInput = document.getElementById('password');
+            const toggleIcon = document.getElementById('toggleIcon');
+
+            // Toggle password visibility
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                toggleIcon.classList.remove('fa-eye-slash');
+                toggleIcon.classList.add('fa-eye');
+            } else {
+                passwordInput.type = 'password';
+                toggleIcon.classList.remove('fa-eye');
+                toggleIcon.classList.add('fa-eye-slash');
+            }
+        });
+    </script>
 </body>
 
 </html>
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,39 +1,44 @@
-<div class="row justify-content-center mt-5">
-    <div class="col-md-8">
+<div class="loginmain">
 
-        <div class="card">
-            <div class="card-header">Please Login to your account</div>
-            <div class="card-body">
-                <form wire:submit="login">
-                    <div class="mb-3 row">
-                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control @error('client_email') is-invalid @enderror" id="email" wire:model="client_email">
-                            @if ($errors->has('client_email'))
-                                <span class="text-danger">{{ $errors->first('client_email') }}</span>
-                            @endif
-                        </div>
+    <div class="logincard">
+        <div class="login-header">Please Login to your account</div>
+        <div class="card-body">
+            <form wire:submit="login">
+                <div class="py-3">
+                    <label for="email" class="text-start">Email Address</label>
+                    <div>
+                        <input type="email" class="form-control @error('client_email') is-invalid @enderror"
+                            id="email" wire:model="client_email">
+                        @if ($errors->has('client_email'))
+                            <span class="text-danger">{{ $errors->first('client_email') }}</span>
+                        @endif
                     </div>
-                    <div class="mb-3 row">
-                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control @error('client_password') is-invalid @enderror" id="password" wire:model="client_password">
-                            @if ($errors->has('client_password'))
-                                <span class="text-danger">{{ $errors->first('client_password') }}</span>
-                            @endif
-                            @if (session()->has('error'))
-                                <span class="text-danger">{{ session('error') }}</span>
-                            @endif
-                        </div>
+                </div>
+                <div>
+                    <label for="password" class="text-start">Password</label>
+                    <div class="eye_posiotin">
+                        <input type="password" class="form-control @error('client_password') is-invalid @enderror"
+                            id="password" wire:model="client_password">
+                        <span id="togglePassword" style="cursor: pointer;">
+                            <i class="fa-regular fa-eye-slash" id="toggleIcon"></i>
+                        </span>
+                        {{-- <i class="fa-regular fa-eye-slash"></i> --}}
+                        {{-- <i class="fa-regular fa-eye-slash"></i> --}}
+                        @if ($errors->has('client_password'))
+                            <span class="text-danger">{{ $errors->first('client_password') }}</span>
+                        @endif
+                        @if (session()->has('error'))
+                            <span class="text-danger">{{ session('error') }}</span>
+                        @endif
                     </div>
-                    <div class="mb-3 row">
-                        <button type="submit" class="col-md-3 offset-md-5 btn btn-primary">
-                            Login
-                        </button>
-                    </div>
+                </div>
+                <div class="pt-3">
+                    <button type="submit" class="w-100 btn btn-primary">
+                        Login
+                    </button>
+                </div>
 
-                </form>
-            </div>
+            </form>
         </div>
     </div>
 </div>
