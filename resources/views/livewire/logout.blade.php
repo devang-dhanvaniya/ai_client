b6c9aabbd670dc05fbf92ebc97438b49231b2849
jenish-devandro <jenish@devandro.in>
2025-02-07 10:19:22 +0530

    design changes

--- resources/views/livewire/logout.blade.php
+++ resources/views/livewire/logout.blade.php
@@ -1,5 +1,1 @@
-<ul class="dropdown-menu">
-    <li>
-        <a class="dropdown-item" href="/logout" wire:click.prevent="logout">Logout</a>
-    </li>
-</ul>
+    <a class="dropdown-item" href="/logout" wire:click.prevent="logout">Logout</a>
--- resources/views/livewire/position-history.blade.php
+++ resources/views/livewire/position-history.blade.php
@@ -1,10 +1,3 @@
 <div class="row mt-3">
-    <div class="col-md-12">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <h5>Data List</h5>
-                <livewire:data-table/>
-            </div>
-        </div>
-    </div>
+    <livewire:data-table />
 </div>
